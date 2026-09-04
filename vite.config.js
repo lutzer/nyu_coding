35608ca068a27c03cdbb5111f9d2fe3c4ae46096
Lutz <lutz@lu-re.de>
2026-09-04 11:07:47 +0200

    adds syntax highlting in code blocks

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import mdx from "@mdx-js/rollup";
+import rehypePrettyCode from "rehype-pretty-code";
 
 export default defineConfig({
   plugins: [
-    { enforce: "pre", ...mdx({ providerImportSource: "@mdx-js/react" }) },
+    {
+      enforce: "pre",
+      ...mdx({
+        providerImportSource: "@mdx-js/react",
+        rehypePlugins: [
+          [rehypePrettyCode, { theme: "github-light", keepBackground: true }],
+        ],
+      }),
+    },
     react(),
   ],
   base: '/nyu_coding/'
