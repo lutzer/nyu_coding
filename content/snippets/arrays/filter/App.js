c190f642feaebd25f0ef9967575a964507b9aeb5
Lutz <lutz@lu-re.de>
2026-09-07 15:31:33 +0200

    adds last lesson of chapter 1

--- content/snippets/arrays/filter/App.js
+++
@@ -1,6 +0,0 @@
-export default function App() {
-  const nums = [1, 2, 3, 4, 5, 6];
-  // TODO: keep only even numbers
-  const evens = nums; // change me
-  return <pre>{JSON.stringify(evens)}</pre>;
-}
